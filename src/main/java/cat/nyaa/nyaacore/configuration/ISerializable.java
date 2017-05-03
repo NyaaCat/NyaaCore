0087cfdcdeba9e440c7b82c179ec9bbf885d40c3
Recursive G <g.recursive@gmail.com>
2017-05-03 14:05:09 -0500

    Nested serialization, close #4

--- src/main/java/cat/nyaa/nyaacore/configuration/ISerializable.java
+++ src/main/java/cat/nyaa/nyaacore/configuration/ISerializable.java
@@ -8,8 +8,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
+/**
+ * Serialize or Deserialize objects into ConfigurationSection
+ */
 public interface ISerializable {
     /**
      * For informative only
@@ -18,19 +24,47 @@ public interface ISerializable {
     public @interface Ephemeral {
     }
 
+    /**
+     * Indicates a field can be serialized.
+     * These four types are supported
+     * 1. Primitive types supported by ConfigurationSection (should put() & get() directly)
+     * 2. ISerializable
+     * //3. List<Type1/2/3/4> (not implemented)
+     * 4. Map<String, Type1/2/3/4>
+     * NOTE:
+     * ENUM without typehint is not supported (ENUM in Map or List) They will be deserialized as string
+     */
     @Target(ElementType.FIELD)
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Serializable {
+        /**
+         * @return the name used in yaml config
+         */
         String name() default "";
 
+        /**
+         * deserializer will check all the alias, but only name will be used for serialization
+         * Usually used for backward compatibility
+         *
+         * @return list of aliases
+         */
         String[] alias() default {};
 
+        /**
+         * When set to true, this field is ignored
+         */
         boolean manualSerialization() default false;
     }
 
+    /**
+     * Must be placed on a {@link FileConfigure} field
+     */
     @Target(ElementType.FIELD)
     @Retention(RetentionPolicy.RUNTIME)
     public @interface StandaloneConfig {
+        /**
+         * When set to true, this field is ignored
+         */
         boolean manualSerialization() default false;
     }
 
@@ -38,78 +72,50 @@ public interface ISerializable {
         deserialize(config, this);
     }
 
+    static void deserialize(ConfigurationSection config, ISerializable serializable) {
+        popSerializable(popPrimitiveMap(config), serializable);
+    }
+
     default void serialize(ConfigurationSection config) {
         serialize(config, this);
     }
 
-    static void deserialize(ConfigurationSection config, Object obj) {
-        Class<?> clz = obj.getClass();
-        for (Field f : clz.getDeclaredFields()) {
-            // standalone config
-            StandaloneConfig standaloneAnno = f.getAnnotation(StandaloneConfig.class);
-            if (standaloneAnno != null && !standaloneAnno.manualSerialization()) {
-                if (FileConfigure.class.isAssignableFrom(f.getType())) {
-                    FileConfigure standaloneCfg = null;
-                    f.setAccessible(true);
-                    try {
-                        standaloneCfg = (FileConfigure) f.get(obj);
-                    } catch (ReflectiveOperationException ex) {
-                        Bukkit.getLogger().log(Level.SEVERE, "Failed to deserialize object", ex);
-                        standaloneCfg = null;
-                    }
-                    if (standaloneCfg != null) {
-                        standaloneCfg.load();
-                        continue;
-                    }
-                }
+    static void serialize(ConfigurationSection config, ISerializable serializable) {
+        pushSerializable(config, serializable);
+    }
+
+    String TYPE_KEY = "__class__";
+
+    // push primitive objects then dispatch complex objects into corresponding methods
+    static void pushMap(ConfigurationSection config, Map<?, ?> map) {
+        for (Map.Entry<?, ?> e : map.entrySet()) {
+            if (!(e.getKey() instanceof String)) {
+                throw new IllegalArgumentException("Map key is not string: " + e.getKey().toString());
             }
-
-            // Normal fields
-            Serializable anno = f.getAnnotation(Serializable.class);
-            if (anno == null || anno.manualSerialization()) continue;
-            f.setAccessible(true);
-            String cfgName = anno.name().equals("") ? f.getName() : anno.name();
-            try {
-                Object origValue = f.get(obj);
-                Object newValue = null;
-                boolean hasValue = false;
-                for (String key : anno.alias()) {
-                    if (config.contains(key)) {
-                        newValue = config.get(key);
-                        hasValue = true;
-                        break;
-                    }
-                }
-                if (!hasValue && config.contains(f.getName())) {
-                    newValue = config.get(f.getName());
-                    hasValue = true;
-                }
-                if (!hasValue && anno.name().length() > 0 && config.contains(anno.name())) {
-                    newValue = config.get(anno.name());
-                    hasValue = true;
-                }
-                if (!hasValue) {
-                    continue;
-                }
-
-                if (f.getType().isEnum()) {
-                    try {
-                        newValue = Enum.valueOf((Class<? extends Enum>) f.getType(), (String) newValue);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                        continue;
-                    }
-                }
-
-                f.set(obj, newValue);
-            } catch (ReflectiveOperationException ex) {
-                Bukkit.getLogger().log(Level.SEVERE, "Failed to deserialize object", ex);
+            String key = (String) e.getKey();
+            Object obj = e.getValue();
+            if (obj == null) continue;
+            if (obj instanceof ISerializable) {
+                ((ISerializable) obj).serialize(config.createSection(key));
+            } else if (obj instanceof Map) {
+                pushMap(config.createSection(key), (Map<?, ?>) obj);
+            } else if (obj instanceof List) {
+                config.set(key, obj);
+                //throw new IllegalArgumentException("List serialization not implemented");
+            } else if (obj instanceof Enum) {
+                config.set(key, ((Enum) obj).name());
+            } else {
+                config.set(key, obj);
             }
         }
     }
 
-    static void serialize(ConfigurationSection config, Object obj) {
-        Class<?> clz = obj.getClass();
+    // Construct shallow map of the object, then call push map.
+    // Also deals with StandaloneConfig
+    static void pushSerializable(ConfigurationSection config, ISerializable serializable) {
+        Class<?> clz = serializable.getClass();
+        Map<String, Object> shallowMap = new HashMap<>();
+        shallowMap.put(TYPE_KEY, clz.getName());
         for (Field f : clz.getDeclaredFields()) {
             // standalone config
             StandaloneConfig standaloneAnno = f.getAnnotation(StandaloneConfig.class);
@@ -118,7 +124,7 @@ public interface ISerializable {
                     FileConfigure standaloneCfg = null;
                     f.setAccessible(true);
                     try {
-                        standaloneCfg = (FileConfigure) f.get(obj);
+                        standaloneCfg = (FileConfigure) f.get(serializable);
                     } catch (ReflectiveOperationException ex) {
                         Bukkit.getLogger().log(Level.SEVERE, "Failed to serialize object", ex);
                         standaloneCfg = null;
@@ -145,16 +151,145 @@ public interface ISerializable {
                 config.set(key, null);
             }
             try {
-                if (f.getType().isEnum()) {
-                    Enum e = (Enum) f.get(obj);
-                    config.set(cfgName, e.name());
-                } else {
-                    Object origValue = f.get(obj);
-                    config.set(cfgName, origValue);
-                }
+                shallowMap.put(cfgName, f.get(serializable));
             } catch (ReflectiveOperationException ex) {
                 Bukkit.getLogger().log(Level.SEVERE, "Failed to serialize object", ex);
             }
+            pushMap(config, shallowMap);
         }
+    }
+
+    // dump all items in the config section into the map, recursively
+    // all returned objects are primitive
+    static Map<String, ?> popPrimitiveMap(ConfigurationSection config) {
+        Map<String, Object> map = new HashMap<>();
+        for (String key : config.getKeys(false)) {
+            if (config.isConfigurationSection(key)) {
+                map.put(key, popPrimitiveMap(config.getConfigurationSection(key)));
+            } else if (config.isList(key)) {
+                map.put(key, config.get(key));
+                //throw new IllegalArgumentException("List deserialization not supported");
+            } else {
+                map.put(key, config.get(key));
+            }
+        }
+        return map;
+    }
+
+    // deserialize ISerializable from primitive map
+    // Also deals with StandaloneConfig
+    static void popSerializable(Map<String, ?> primitiveMap, ISerializable serializable) {
+        Class<?> clz = serializable.getClass();
+        for (Field f : clz.getDeclaredFields()) {
+            // standalone config
+            StandaloneConfig standaloneAnno = f.getAnnotation(StandaloneConfig.class);
+            if (standaloneAnno != null && !standaloneAnno.manualSerialization()) {
+                if (FileConfigure.class.isAssignableFrom(f.getType())) {
+                    FileConfigure standaloneCfg = null;
+                    f.setAccessible(true);
+                    try {
+                        standaloneCfg = (FileConfigure) f.get(serializable);
+                    } catch (ReflectiveOperationException ex) {
+                        Bukkit.getLogger().log(Level.SEVERE, "Failed to deserialize object", ex);
+                        standaloneCfg = null;
+                    }
+                    if (standaloneCfg != null) {
+                        standaloneCfg.load();
+                        continue;
+                    }
+                }
+            }
+
+            // Normal fields
+            Serializable anno = f.getAnnotation(Serializable.class);
+            if (anno == null || anno.manualSerialization()) continue;
+            f.setAccessible(true);
+            String cfgName = anno.name().equals("") ? f.getName() : anno.name();
+            try {
+                Object origValue = f.get(serializable);
+                Object newPrimitiveValue = null;
+                boolean hasValue = false;
+                for (String key : anno.alias()) {
+                    if (primitiveMap.containsKey(key)) {
+                        newPrimitiveValue = primitiveMap.get(key);
+                        hasValue = true;
+                        break;
+                    }
+                }
+                if (!hasValue && primitiveMap.containsKey(f.getName())) {
+                    newPrimitiveValue = primitiveMap.get(f.getName());
+                    hasValue = true;
+                }
+                if (!hasValue && anno.name().length() > 0 && primitiveMap.containsKey(anno.name())) {
+                    newPrimitiveValue = primitiveMap.get(anno.name());
+                    hasValue = true;
+                }
+                if (!hasValue) {
+                    continue;
+                }
+
+                Object newValue = null;
+                if (Map.class.isAssignableFrom(f.getType())) {
+                    if (!(newPrimitiveValue instanceof Map))
+                        throw new IllegalArgumentException("Config field require a map object: " + f.toString());
+                    newValue = constructNonPrimitiveMap((Map) newPrimitiveValue);
+                } else if (ISerializable.class.isAssignableFrom(f.getType())) {
+                    if (!(newPrimitiveValue instanceof Map))
+                        throw new IllegalArgumentException("Config field require a map object: " + f.toString());
+                    ISerializable obj;
+                    try {
+                        obj = (ISerializable) f.getType().newInstance();
+                    } catch (ReflectiveOperationException ex) {
+                        throw new IllegalArgumentException(ex);
+                    }
+                    popSerializable((Map) newPrimitiveValue, obj);
+                    newValue = obj;
+                } else if (f.getType().isEnum()) {
+                    if (!(newPrimitiveValue instanceof String))
+                        throw new IllegalArgumentException("Config field require a string object: " + f.toString());
+                    newValue = Enum.valueOf((Class<? extends Enum>) f.getType(), (String) newPrimitiveValue);
+                } else {
+                    newValue = newPrimitiveValue;
+                }
+
+                f.set(serializable, newValue);
+            } catch (ReflectiveOperationException ex) {
+                Bukkit.getLogger().log(Level.SEVERE, "Failed to deserialize object", ex);
+            }
+        }
+    }
+
+    // convert primitive objects returned by popPrimitiveMap() into actual objects
+    // also recursively reconstruct all the elements in list or map
+    // e.g. Map -> ISerializable
+    static Object constructNonPrimitiveMap(Map primitiveMap) {
+        if (primitiveMap.containsKey(TYPE_KEY)) {
+            Class<?> type;
+            try {
+                type = Class.forName((String) primitiveMap.get(TYPE_KEY));
+                if (!ISerializable.class.isAssignableFrom(type)) {
+                    throw new IllegalArgumentException("Bad class name: " + primitiveMap.get(TYPE_KEY));
+                }
+                ISerializable obj = (ISerializable) type.newInstance();
+                ISerializable.popSerializable(primitiveMap, obj);
+                return obj;
+            } catch (ReflectiveOperationException ex) {
+                throw new IllegalArgumentException(ex);
+            }
+        }
+
+        Map<Object, Object> nonPrimitiveMap = new HashMap<>();
+        for (Object key : primitiveMap.keySet()) {
+            Object obj = primitiveMap.get(key);
+            if (obj instanceof List) {
+                nonPrimitiveMap.put(key, obj);
+                //throw new IllegalArgumentException("List deserialization not supported");
+            } else if (obj instanceof Map) {
+                nonPrimitiveMap.put(key, constructNonPrimitiveMap((Map) obj));
+            } else {
+                nonPrimitiveMap.put(key, obj);
+            }
+        }
+        return nonPrimitiveMap;
     }
 }
---
+++ src/test/java/cat/nyaa/nyaacore/configuration/ISerializableTest.java
@@ -0,0 +1,29 @@
+package cat.nyaa.nyaacore.configuration;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.junit.Test;
+
+import java.io.StringReader;
+
+import static org.junit.Assert.assertEquals;
+
+public class ISerializableTest {
+    @Test
+    public void test1() throws Exception {
+        YamlConfiguration cfg = new YamlConfiguration();
+        DataObject obj1 = new DataObject();
+        DataObject obj2 = new DataObject();
+        obj1.fillNestedList();
+        obj1.serialize(cfg);
+        String yml = cfg.saveToString();
+        final String ans = "str: The quick brown fox jumps over the lazy dog\n__class__: cat.nyaa.nyaacore.configuration.DataObject\nnumber_int: 42\nnumber_long: 42\nenum_1: VALUE_A\nenum_2: LEAVES\nmap:\n  key1: value1\n  key2: value2\n  key3: value3\n  key4: value4\nanotherDataObject:\n  x: 42\n  __class__: cat.nyaa.nyaacore.configuration.DataObject$NestedObject\n  d: 42.42\nmapOfDataObject:\n  obj2:\n    x: 42\n    __class__: cat.nyaa.nyaacore.configuration.DataObject$NestedObject\n    d: 42.42\n  obj1:\n    x: 42\n    __class__: cat.nyaa.nyaacore.configuration.DataObject$NestedObject\n    d: 42.42\nnestedList:\n  '0':\n  - 0\n  - 1\n  - 2\n  - 3\n  - 4\n  - 5\n  - 6\n  - 7\n  - 8\n  - 9\n  '11':\n  - 30\n  - 31\n  - 32\n  - 33\n  - 34\n  - 35\n  - 36\n  - 37\n  - 38\n  - 39\n  '110':\n  - 60\n  - 61\n  - 62\n  - 63\n  - 64\n  - 65\n  - 66\n  - 67\n  - 68\n  - 69\n  '1':\n  - 10\n  - 11\n  - 12\n  - 13\n  - 14\n  - 15\n  - 16\n  - 17\n  - 18\n  - 19\n  '100':\n  - 40\n  - 41\n  - 42\n  - 43\n  - 44\n  - 45\n  - 46\n  - 47\n  - 48\n  - 49\n  '111':\n  - 70\n  - 71\n  - 72\n  - 73\n  - 74\n  - 75\n  - 76\n  - 77\n  - 78\n  - 79\n  '101':\n  - 50\n  - 51\n  - 52\n  - 53\n  - 54\n  - 55\n  - 56\n  - 57\n  - 58\n  - 59\n  '1001':\n  - 90\n  - 91\n  - 92\n  - 93\n  - 94\n  - 95\n  - 96\n  - 97\n  - 98\n  - 99\n  '1000':\n  - 80\n  - 81\n  - 82\n  - 83\n  - 84\n  - 85\n  - 86\n  - 87\n  - 88\n  - 89\n  '10':\n  - 20\n  - 21\n  - 22\n  - 23\n  - 24\n  - 25\n  - 26\n  - 27\n  - 28\n  - 29\n";
+        assertEquals(ans, yml);
+
+        YamlConfiguration parse = YamlConfiguration.loadConfiguration(new StringReader(yml));
+        obj2.deserialize(parse);
+        for (int i=0;i<=9;i++) {
+            for (int j=0;j<=9;j++)
+                assertEquals((long)(i*10+j), (long)obj2.nestedList.get(Integer.toString(i,2)).get(j));
+        }
+    }
+}
