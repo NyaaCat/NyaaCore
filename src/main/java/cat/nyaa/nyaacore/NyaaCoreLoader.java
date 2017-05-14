1f92c1daab475fb425942beb8a6134d2939a5cd8
Librazy <librazy@foxmail.com>
2017-05-15 02:03:51 +0800

    init L10nUtils

--- src/main/java/cat/nyaa/nyaacore/NyaaCoreLoader.java
+++ src/main/java/cat/nyaa/nyaacore/NyaaCoreLoader.java
@@ -1,11 +1,13 @@
 package cat.nyaa.nyaacore;
 
+import cat.nyaa.nyaacore.utils.L10nUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NyaaCoreLoader extends JavaPlugin {
     @Override
     public void onLoad() {
         LanguageRepository.initInternalMap(this);
+        L10nUtils.init(this);
     }
 
     @Override
