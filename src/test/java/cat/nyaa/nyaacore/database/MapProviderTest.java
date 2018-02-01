36c2c00dbaf80cd44b971277552849c068fd8688
Librazy <im@librazy.org>
2018-02-01 17:19:50 +0800

    fix test

--- src/test/java/cat/nyaa/nyaacore/database/MapProviderTest.java
+++ src/test/java/cat/nyaa/nyaacore/database/MapProviderTest.java
@@ -6,13 +6,15 @@ import org.junit.Test;
 public class MapProviderTest {
     @Test
     public void test1() {
-        KeyValueDB<Integer, String> db = DatabaseUtils.get("map", null);
+        KeyValueDB<Integer, String> db = DatabaseUtils.get(null, "map", null);
+        db.connect();
         Assert.assertNull(db.get(1));
     }
 
     @Test
     public void test2() {
-        KeyValueDB<String, String> db = DatabaseUtils.get("map", null);
+        KeyValueDB<String, String> db = DatabaseUtils.get(null,  "map", null);
+        db.connect();
         db.put("s", "t");
         db.put("k", "v");
         Assert.assertEquals("v", db.get("k"));
@@ -20,7 +22,8 @@ public class MapProviderTest {
 
     @Test
     public void test3() {
-        KeyValueDB<String, String> db = DatabaseUtils.get("map", null);
+        KeyValueDB<String, String> db = DatabaseUtils.get(null, "map", null);
+        db.connect();
         db.put("k", "v");
         db.asMap().clear();
         Assert.assertNull(db.get("k"));
