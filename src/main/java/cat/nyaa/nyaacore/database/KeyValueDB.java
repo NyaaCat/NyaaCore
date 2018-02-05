c4c62462eedb88d6c34f5fd9fe5eaac2786e475f
Librazy <im@librazy.org>
2018-02-05 19:26:33 +0800

    some async api with default impl

--- src/main/java/cat/nyaa/nyaacore/database/KeyValueDB.java
+++ src/main/java/cat/nyaa/nyaacore/database/KeyValueDB.java
@@ -2,19 +2,41 @@ package cat.nyaa.nyaacore.database;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 
 public interface KeyValueDB<K, V> extends Database{
 
     V get(K key);
 
+    default Future<V> getAsync(K key) {
+        return CompletableFuture.supplyAsync(() -> get(key));
+    }
+
     V get(K key, Function<? super K,? extends V> mappingFunction);
+
+    default Future<V> getAsync(K key, Function<? super K,? extends V> mappingFunction) {
+        return CompletableFuture.supplyAsync(() -> get(key, mappingFunction));
+    }
 
     void put(K key, V value);
 
+    default Future<Void> putAsync(K key, V value) {
+        return CompletableFuture.supplyAsync(() -> {put(key, value);return null;});
+    }
+
     Collection<V> getAll(K key);
 
-    Map<K,V> asMap();
+    default Future<Collection<V>> getAllAsync(K key) {
+        return CompletableFuture.supplyAsync(() -> getAll(key));
+    }
+
+    Map<K, V> asMap();
 
     void clear();
+
+    default Future<Void> clearAsync() {
+        return CompletableFuture.supplyAsync(() -> {clear();return null;});
+    }
 }
--- src/main/java/cat/nyaa/nyaacore/database/Query.java
+++ src/main/java/cat/nyaa/nyaacore/database/Query.java
@@ -1,6 +1,8 @@
 package cat.nyaa.nyaacore.database;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 public interface Query<T> {
 
@@ -12,13 +14,41 @@ public interface Query<T> {
 
     void delete();
 
+    default Future<Void> deleteAsync()
+    {
+        delete();
+        return CompletableFuture.completedFuture(null);
+    }
+
     void insert(T object);
+
+    default Future<Void> insertAsync(T object)
+    {
+        insert(object);
+        return CompletableFuture.completedFuture(null);
+    }
 
     List<T> select();
 
+    default Future<List<T>> selectAsync() {
+        return CompletableFuture.supplyAsync(this::select);
+    }
+
     T selectUnique();
+
+    default Future<T> selectUniqueAsync() {
+        return CompletableFuture.supplyAsync(this::selectUnique);
+    }
 
     int count();
 
+    default Future<Integer> countAsync() {
+        return CompletableFuture.supplyAsync(this::count);
+    }
+
     void update(T obj, String... columns);
+
+    default Future<Void> updateAsync(T obj, String... columns) {
+        return CompletableFuture.supplyAsync(() -> {update(obj, columns); return null;});
+    }
 }
