4e780807face5011a3efd938ba6f1027822f78ce
Recursive G <g.recursive@gmail.com>
2018-07-29 14:00:05 +0800

    added ConcurrentUtils

---
+++ src/main/java/cat/nyaa/nyaacore/utils/ConcurrentUtils.java
@@ -0,0 +1,23 @@
+package cat.nyaa.nyaacore.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public final class ConcurrentUtils {
+    public static <P,Q> void runAsyncTask(Plugin plugin, P parameter, Function<P,Q> asyncTask, Consumer<Q> callback) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ()->{
+            final Q ret = asyncTask.apply(parameter);
+            Bukkit.getScheduler().runTask(plugin, ()->{
+                callback.accept(ret);
+            });
+        });
+    }
+
+    public static <P> void runAsyncTask(Plugin plugin, P parameter, Consumer<P> asyncTask) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ()->{
+            asyncTask.accept(parameter);
+        });
+    }
+}
