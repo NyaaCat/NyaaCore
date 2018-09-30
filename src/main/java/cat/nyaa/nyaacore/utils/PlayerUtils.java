8fb132942ab2843b893099920d6dc2ab0786f819
cyilin <cyilin@users.noreply.github.com>
2018-09-30 11:28:51 +0800

    support open written book NyaaCat/NyaaUtils#56

---
+++ src/main/java/cat/nyaa/nyaacore/utils/PlayerUtils.java
@@ -0,0 +1,35 @@
+package cat.nyaa.nyaacore.utils;
+
+import net.minecraft.server.v1_13_R2.EntityPlayer;
+import net.minecraft.server.v1_13_R2.EnumHand;
+import net.minecraft.server.v1_13_R2.PacketPlayOutSetSlot;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerUtils {
+    public static void openWrittenBook(Player player, ItemStack book) {
+        if (book != null && book.getType() == Material.WRITTEN_BOOK) {
+            sendFakeItemStack(player, player.getInventory().getHeldItemSlot(), book);
+            ((CraftPlayer) player).getHandle().a(CraftItemStack.asNMSCopy(book), EnumHand.MAIN_HAND);
+            player.updateInventory();
+        }
+    }
+
+    public static void sendFakeItemStack(Player p, int index, ItemStack itemStack) {
+        EntityPlayer player = ((CraftPlayer) p).getHandle();
+        if (player.playerConnection == null) {
+            return;
+        }
+        if (index < net.minecraft.server.v1_13_R2.PlayerInventory.getHotbarSize()) {
+            index += 36;
+        } else if (index > 39) {
+            index += 5;
+        } else if (index > 35) {
+            index = 8 - (index - 36);
+        }
+        player.playerConnection.sendPacket(new PacketPlayOutSetSlot(player.defaultContainer.windowId, index, CraftItemStack.asNMSCopy(itemStack)));
+    }
+}
