af719df5afda2b9f6450ac12280575aa9328816b
Librazy <im@librazy.org>
2018-12-03 01:04:51 +0800

    Add trident damage dealt accessor

--- src/main/java/cat/nyaa/nyaacore/utils/TridentUtils.java
+++ src/main/java/cat/nyaa/nyaacore/utils/TridentUtils.java
@@ -13,11 +13,14 @@ public final class TridentUtils {
     private static Class<?> entityThrownTrident = ReflectionUtils.getNMSClass("EntityThrownTrident");
 
     private static Field entityThrownTridentFieldAw;
+    private static Field entityThrownTridentFieldAx;
 
     static {
         try {
             entityThrownTridentFieldAw = entityThrownTrident.getDeclaredField("aw");
             entityThrownTridentFieldAw.setAccessible(true);
+            entityThrownTridentFieldAx = entityThrownTrident.getDeclaredField("ax");
+            entityThrownTridentFieldAx.setAccessible(true);
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
@@ -38,6 +41,24 @@ public final class TridentUtils {
             EntityThrownTrident thrownTrident = (EntityThrownTrident) ((CraftEntity) entity).getHandle();
             net.minecraft.server.v1_13_R2.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
             entityThrownTridentFieldAw.set(thrownTrident, nmsItemStack);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean getTridentDealtDamage(Trident entity) {
+        try {
+            EntityThrownTrident thrownTrident = (EntityThrownTrident) ((CraftEntity) entity).getHandle();
+            return (boolean) entityThrownTridentFieldAx.get(thrownTrident);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setTridentDealtDamage(Trident entity, boolean dealtDamage) {
+        try {
+            EntityThrownTrident thrownTrident = (EntityThrownTrident) ((CraftEntity) entity).getHandle();
+            entityThrownTridentFieldAx.set(thrownTrident, dealtDamage);
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
