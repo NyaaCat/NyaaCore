a7e37a37527be3e4b5b06f74fccc8a168db8b937
ReinWD <ReinWDD@gmail.com>
2020-02-28 21:03:52 +0800

    save tag to item

--- src/main/java/cat/nyaa/nyaacore/utils/ItemTagUtils.java
+++ src/main/java/cat/nyaa/nyaacore/utils/ItemTagUtils.java
@@ -21,8 +21,10 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_STRING) ? Optional.empty() : Optional.of(tag.getString(key));
     }
     public static Optional<String> setString(ItemStack item, String key, String value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         tag.setString(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -32,9 +34,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getInt(key));
     }
     public static Optional<Integer> setInt(ItemStack item, String key, int value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setInt(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -45,9 +49,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getDouble(key));
     }
     public static Optional<Double> setDouble(ItemStack item, String key, double value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setDouble(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -58,9 +64,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getShort(key));
     }
     public static Optional<Short> setShort(ItemStack item, String key, short value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setShort(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -71,9 +79,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getByte(key));
     }
     public static Optional<Byte> setByte(ItemStack item, String key, byte value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setByte(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -84,9 +94,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getLong(key));
     }
     public static Optional<Long> setLong(ItemStack item, String key, long value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setLong(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -97,9 +109,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_LONG_ARRAY) ? Optional.empty() : Optional.of(tag.getLongArray(key));
     }
     public static Optional<long[]> setLongArray(ItemStack item, String key, long[] value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.a(key, value); // this is anonymous
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -110,9 +124,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_INT_ARRAY) ? Optional.empty() : Optional.of(tag.getIntArray(key));
     }
     public static Optional<int[]> setIntArray(ItemStack item, String key, int[] value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setIntArray(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -123,9 +139,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_BYTE_ARRAY) ? Optional.empty() : Optional.of(tag.getByteArray(key));
     }
     public static Optional<byte[]> setByteArray(ItemStack item, String key, byte[] value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setByteArray(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -136,9 +154,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getBoolean(key));
     }
     public static Optional<Boolean> setBoolean(ItemStack item, String key, boolean value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setBoolean(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 
@@ -149,9 +169,11 @@ public class ItemTagUtils {
         return !tag.hasKeyOfType(key, TYPE_NUMBER) ? Optional.empty() : Optional.of(tag.getFloat(key));
     }
     public static Optional<Float> setFloat(ItemStack item, String key, float value){
-        NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
+        net.minecraft.server.v1_15_R1.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = itemStack.getOrCreateTag();
         if (tag == null) return Optional.empty();
         tag.setFloat(key, value);
+        itemStack.setTag(tag);
         return Optional.of(value);
     }
 }
