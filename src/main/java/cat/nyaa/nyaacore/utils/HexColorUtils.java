213f47318b98d2c98e3f5b624d1c1bead4f836a4
ReinWD <ReinWDD@gmail.com>
2020-09-12 17:12:34 +0800

    support hex color

---
+++ src/main/java/cat/nyaa/nyaacore/utils/HexColorUtils.java
@@ -0,0 +1,94 @@
+package cat.nyaa.nyaacore.utils;
+
+import net.milkbowl.vault.chat.Chat;
+import org.bukkit.ChatColor;
+
+import java.awt.*;
+import java.util.EnumSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HexColorUtils {
+    //Extracted from EssentialsX
+    private static final Pattern REPLACE_ALL_RGB_PATTERN = Pattern.compile("(&)?&#([0-9a-fA-F]{6})");
+    private static final Pattern REPLACE_ALL_PATTERN = Pattern.compile("(&)?&([0-9a-fk-orA-FK-OR])");
+
+    private static String replaceFormat(final String input) {
+        if (input == null) {
+            return null;
+        }
+        return replaceColor(input, EnumSet.allOf(ChatColor.class), true);
+    }
+
+    private static String replaceColor(final String input, final Set<ChatColor> supported, boolean rgb) {
+        StringBuffer legacyBuilder = new StringBuffer();
+        Matcher legacyMatcher = REPLACE_ALL_PATTERN.matcher(input);
+        legacyLoop: while (legacyMatcher.find()) {
+            boolean isEscaped = (legacyMatcher.group(1) != null);
+            if (!isEscaped) {
+                char code = legacyMatcher.group(2).toLowerCase(Locale.ROOT).charAt(0);
+                for (ChatColor color : supported) {
+                    if (color.getChar() == code) {
+                        legacyMatcher.appendReplacement(legacyBuilder, "\u00a7$2");
+                        continue legacyLoop;
+                    }
+                }
+            }
+            // Don't change & to section sign (or replace two &'s with one)
+            legacyMatcher.appendReplacement(legacyBuilder, "&$2");
+        }
+        legacyMatcher.appendTail(legacyBuilder);
+
+        if (rgb) {
+            StringBuffer rgbBuilder = new StringBuffer();
+            Matcher rgbMatcher = REPLACE_ALL_RGB_PATTERN.matcher(legacyBuilder.toString());
+            while (rgbMatcher.find()) {
+                boolean isEscaped = (rgbMatcher.group(1) != null);
+                if (!isEscaped) {
+                    try {
+                        String hexCode = rgbMatcher.group(2);
+                        rgbMatcher.appendReplacement(rgbBuilder, parseHexColor(hexCode));
+                        continue;
+                    } catch (NumberFormatException ignored) {
+                    }
+                }
+                rgbMatcher.appendReplacement(rgbBuilder, "&#$2");
+            }
+            rgbMatcher.appendTail(rgbBuilder);
+            return rgbBuilder.toString();
+        }
+        return legacyBuilder.toString();
+    }
+
+    /**
+     * @throws NumberFormatException If the provided hex color code is invalid or if version is lower than 1.16.
+     */
+    private static String parseHexColor(String hexColor) throws NumberFormatException {
+        if (hexColor.startsWith("#")) {
+            hexColor = hexColor.substring(1); //fuck you im reassigning this.
+        }
+        if (hexColor.length() != 6) {
+            throw new NumberFormatException("Invalid hex length");
+        }
+        Color.decode("#" + hexColor);
+        StringBuilder assembledColorCode = new StringBuilder();
+        assembledColorCode.append("\u00a7x");
+        for (char curChar : hexColor.toCharArray()) {
+            assembledColorCode.append("\u00a7").append(curChar);
+        }
+        return assembledColorCode.toString();
+    }
+    //Extracted from EssentialsX    
+
+    //actual call
+    public static String hexColored(String str){
+        try{
+            return replaceFormat(str);
+        } catch (Exception e){
+            //fallback in case an exception thrown.
+            return ChatColor.translateAlternateColorCodes('&', str);
+        }
+    }
+}
