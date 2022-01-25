e48f055e6b5c295dc48e352af673ae4dc88c79ba
Takaranoao <takaranoao@gmail.com>
2022-01-26 01:02:56 +0800

    new method:EntityUtils.getEntityTypeId

--- src/main/java/cat/nyaa/nyaacore/utils/EntityUtils.java
+++ src/main/java/cat/nyaa/nyaacore/utils/EntityUtils.java
@@ -1,8 +1,12 @@
 package cat.nyaa.nyaacore.utils;
 
+import net.minecraft.core.IRegistry;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityTypes;
+import org.bukkit.entity.EntityType;
 
 import java.lang.reflect.Field;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class EntityUtils {
@@ -12,7 +16,13 @@ public class EntityUtils {
         try {
             Field ENTITY_COUNTER = Entity.class.getDeclaredField("ENTITY_COUNTER");
             return ((AtomicInteger) ENTITY_COUNTER.get(null)).incrementAndGet();
-        } catch (IllegalAccessException | NoSuchFieldException ignored) {}
+        } catch (IllegalAccessException | NoSuchFieldException ignored) {
+        }
         return -FAKE_ENTITY_COUNTER.incrementAndGet();
+    }
+
+    public static Optional<Integer> getEntityTypeId(EntityType entityType) {
+        Optional<EntityTypes<?>> entityTypesOptional = EntityTypes.byString(entityType.getKey().getKey());
+        return entityTypesOptional.map(IRegistry.ENTITY_TYPE::getId);
     }
 }
